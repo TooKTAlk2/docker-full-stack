2e7c0a8df3336374c5a6fc08e5f01c0414f12021
TookTalk2Mac <wootaejung24@gmail.com>
2020-12-03 00:23:24 +0900

    trvais ci deploy

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,13 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host : 'mysql',
-  user : 'root',
-  password : 'dk99521',
-  database : 'myapp'
+  host : process.env.MYSQL_HOST,
+  user : process.env.MYSQL_USER,
+  password : process.env.MYSQL_ROOT_PASSWORD,
+  database : process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT
 
 });
 
-// 내보내기
+
 exports.pool = pool;
